7c85d85d323a347cc9322f8632da3fe8510c5280
mr-fool <mr-fool@users.noreply.github.com>
2019-07-10 17:04:15 -0600

    tons of changes

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component{
       {
         id: 2,
         title: "Take out the piss bottle",
-        completed: false
+        completed: true
       },
       {
         id: 3,
@@ -22,10 +22,15 @@ class App extends Component{
       }
     ]
   }
+
+  markComplete = () => {
+    console.log("From app.js")
+  }
+
   render() {
     return (
       <div className="App">
-        <Todos todos={this.state.todos}/>
+        <Todos todos={this.state.todos} markComplete={this.markComplete}/>
       </div>
     );
   }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -5,19 +5,30 @@ export class TodoItem extends Component {
     getStyle = () => {
         if (this.props.todo.completed) {
             return {
+                background: '#f4f4f4',
+                padding: "10px",
+                borderBottom: '1px #ccc dotted',
                 textDecoration: 'line-through'
             }
         }
         else {
             return {
+                background: '#f4f4f4',
+                padding: "10px",
+                borderBottom: '1px #ccc dotted',
                 textDecoration: 'none'
             }
         }
     }
+
+
     render() {
         return (
             <div style={this.getStyle()}>
-                <p>{this.props.todo.title}</p>
+                <p>
+                    <input type="checkbox" onChange={this.props.markComplete.bind(this,this.props.todo.id)}/> {" "}
+                    {this.props.todo.title}
+                </p>
             </div>
         )
     }
