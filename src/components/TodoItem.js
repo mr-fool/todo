500af9132c695ad0709387628b617c877bbfdf14
mr-fool <mr-fool@users.noreply.github.com>
2019-07-10 01:16:56 -0600

    building

---
+++ src/components/TodoItem.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+import PropTypes from "prop-types";
+
+export class TodoItem extends Component {
+    getStyle = () => {
+        
+    }
+    render() {
+        return (
+            <div style={this.getStyle()}>
+                <p>{this.props.todo.title}</p>
+            </div>
+        )
+    }
+}
+//PropTypes
+TodoItem.propTypes = {
+    todo: PropTypes.object.isRequired
+}
+export default TodoItem
