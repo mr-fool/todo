abc9773ba32dd852c9933219264db27f67a2430b
mr-fool <mr-fool@users.noreply.github.com>
2019-07-10 20:52:31 -0600

    add

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Todos from './components/Todo';
+import Header from './components/layout/Header';
 import './App.css';
 
 class App extends Component{
@@ -41,6 +42,7 @@ class App extends Component{
   render() {
     return (
       <div className="App">
+        <Header/>
         <Todos todos={this.state.todos} markComplete={this.markComplete}
         delTodo={this.delTodo}/>
       </div>
