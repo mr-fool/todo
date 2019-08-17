4faafe361bc169350bd27ac8815acd03b8144cef
mr-fool <mr-fool@users.noreply.github.com>
2019-08-17 16:03:21 -0600

    build

---
+++ build/precache-manifest.6527b27073fa50fcfe35c9bfe8ca15ea.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19f863d85a77715d999e32b3ba176f3a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7adad3adaa27aa1ac773",
+    "url": "/static/css/main.9cdfa84b.chunk.css"
+  },
+  {
+    "revision": "c15184ca73cbbaf4a5da",
+    "url": "/static/js/2.7c8086d2.chunk.js"
+  },
+  {
+    "revision": "7adad3adaa27aa1ac773",
+    "url": "/static/js/main.e225f15b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
